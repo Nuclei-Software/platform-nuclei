85d0363057ffc9a9086387d7a5480abefd1c6c52
Huaqi Fang <578567190@qq.com>
2023-12-29 22:19:03 +0800

    ci: flush output in time
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- .github/build_examples.py
+++ .github/build_examples.py
@@ -8,11 +8,15 @@ for example in os.listdir("examples"):
     print("Use PIO to build %s" % (example))
     pio_cmd = "pio run -d examples/%s" %(example)
     print(pio_cmd)
+    sys.stdout.flush()
     ret = os.system(pio_cmd)
     if ret != 0:
         print("%s build fail!" % (example))
+        sys.stdout.flush()
         PASS = False
         Fails.append(example)
+    else:
+        print("%s build successfully!" % (example))
 
 if PASS:
     print("All examples compiled passed!")
